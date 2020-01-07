0094010092fab60908c22993e272a3fea8f32d5a
GuGU <wnduddl3409@naver.com>
2020-01-07 13:09:31 +0900

    [Update] sendMail Function with Nodemailer

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import { sendSecretMail } from "./utils";
+
+sendSecretMail("sprnd645@gmail.com", "123");
 
 const PORT = process.env.PORT || 4000;
 
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,29 @@
-import { adjectives, nouns } from "./lib"
+import { adjectives, nouns } from "./lib";
+import nodemailer from "nodemailer";
+import sgTranport from "nodemailer-sendgrid-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  }
+  const client = nodemailer.createTransport(sgTranport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "Prismagram",
+    to: address,
+    subject: "🔒Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`
+  };
+  return sendMail(email);
 };
