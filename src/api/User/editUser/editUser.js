48eeed2bc16310b0f73ba4956ede86a8bffd0d7e
GuGU <wnduddl3409@naver.com>
2020-01-07 17:58:53 +0900

    [Create] user editUser API

---
+++ src/api/User/editUser/editUser.js
@@ -0,0 +1,16 @@
+import { isAuthenticated } from "src/middlewares";
+import { prisma } from "generated/prisma-client";
+
+export default {
+  Mutation: {
+    editUser: (_, args, { request }) => {
+      isAuthenticated(request);
+      const { username, email, firstName, lastName, bio } = args;
+      const { user } = request;
+      return prisma.updateUser({
+        where: { id: user.id },
+        data: { username, email, firstName, lastName, bio }
+      });
+    }
+  }
+}
