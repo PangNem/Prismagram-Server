ae556e3a380acd7cf89ae83000a94a228cda2c92
GuGU <wnduddl3409@naver.com>
2020-01-10 01:26:59 +0900

    [Create] me

---
+++ src/api/User/me/me.js
@@ -0,0 +1,18 @@
+import { isAuthenticated } from "src/middlewares";
+import { prisma } from "generated/prisma-client";
+
+export default {
+  Query: {
+    me: async (_, __, { request }) => {
+      isAuthenticated(request);
+      const { user } = request
+      const userProfile = await prisma.user({ id: user.id });
+      const posts = await prisma.user({ id: user.id }).posts();
+      console.log(userProfile);
+      console.log(posts);
+      return {
+        userProfile, posts
+      };
+    }
+  }
+}
