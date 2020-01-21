0b9be404695d5f577b1ae0bca31b3ab3b1ad129b
GuGU <wnduddl3409@naver.com>
2020-01-21 20:20:36 +0900

    [Update] kill full post fragment

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,12 +1,10 @@
 import { prisma } from 'generated/prisma-client';
-import { FULL_POST_FRAGMENT } from 'src/fragments';
 
 export default {
   Query: {
-    seeFullPost: async (_, args) => {
+    seeFullPost: (_, args) => {
       const { id } = args;
-
-      return await prisma.post({ id }).$fragment(FULL_POST_FRAGMENT);
+      return prisma.post({ id });
     }
   }
 }
--- src/fragments.js
+++ src/fragments.js
@@ -29,23 +29,6 @@ export const MESSAGE_FRAGMENT = `
   }
 `;
 
-export const FULL_POST_FRAGMENT = `
-  fragment PostParts on Post {
-    id
-    caption
-    location
-    user {
-      ${USER_FRAGMENT}
-    }
-    comments {
-      ${COMMENT_FRAGMENT}
-    }
-    files {
-      ${FILE_FRAGMENT}
-    }
-  }
-`;
-
 export const ROOM_FRAGMENT = `
   fragment RoomParts on Room {
     id
